d85a6e5b4b89fdc3ef868e17f540cca36fb68e92
damboy0 <fagbolade006@gmail.com>
2024-09-18 19:24:54 +0100

    add MerkleAirdrop Test

--- test/MerkleAirdrop.ts
+++ test/MerkleAirdrop.ts
@@ -7,19 +7,19 @@ import { generateMerkleTree } from "../scripts/Merkle";
 import path from "path";
 
 describe("MerkleAirdrop", function () {
-  // Fixture to deploy the token contract
+  
   async function deployToken() {
-    const owner = "0xf584F8728B874a6a5c7A8d4d387C9aae9172D621"; // Owner address
-    const claimer1 = "0xF22742F06e4F6d68A8d0B49b9F270bB56affAB38"; // Claimer address
+    const owner = "0xf584F8728B874a6a5c7A8d4d387C9aae9172D621"; 
+    const claimer1 = "0xF22742F06e4F6d68A8d0B49b9F270bB56affAB38"; 
 
-    // Impersonate the owner and claimer accounts
+   
     await helpers.impersonateAccount(owner);
     await helpers.impersonateAccount(claimer1);
     
     const ownerSigner = await ethers.getSigner(owner);
     const claimer1Signer = await ethers.getSigner(claimer1);
 
-    // Deploy ERC20 mock token
+    
     const Token = await ethers.getContractFactory("ERC20Mock");
     const roccoToken = await Token.connect(ownerSigner).deploy(
       "Damboy Token",
@@ -31,7 +31,7 @@ describe("MerkleAirdrop", function () {
     return { roccoToken, ownerSigner, claimer1Signer };
   }
 
-  // Fixture to deploy the MerkleAirdrop contract
+
   async function deployMerkleAirdrop() {
     const { roccoToken, ownerSigner, claimer1Signer } = await loadFixture(deployToken);
 
@@ -43,7 +43,7 @@ describe("MerkleAirdrop", function () {
     const merkleAirdrop = await MerkleAirdrop.connect(ownerSigner).deploy(
       roccoToken.address,
       root,
-      "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D" // BAYC address
+      "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D" 
     );
 
     return { roccoToken, merkleAirdrop, ownerSigner, claimer1Signer, proofs };
